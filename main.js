0d7cbf118b320b5996abf9994050aa20930c83d5
toothlessdev <toothless042@gmail.com>
2023-03-28 23:04:58 +0900

    final commit

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+this.document.querySelector(".main-container").style.transform = "translateY(0px)";
+this.document.querySelector(".main-container").style.opacity = 1;
+
 const MIN_INDEX = 0;
 const MAX_INDEX = 2;
 let currentIndex = 0;
@@ -14,64 +17,71 @@ document.querySelector("#next-btn").addEventListener("click", function () {
     document.querySelector(".music-container").style.transform = "translateX(" + currentIndex * -100 + "vw)";
 });
 
-// Scroll Behavior
-
-function Mapping(scroll_begin, scroll_end, value_begin, value_end, scroll_y) {
-    return (scroll_y * (value_end - value_begin)) / (scroll_end - scroll_begin) + value_begin - ((value_end - value_begin) * scroll_begin) / (scroll_end - scroll_begin);
-}
-
-let triggered = false;
-// window.addEventListener("scroll", function () {
-//     //console.log(scrollY);
-
-//     if (350 > scrollY) {
-//         this.document.querySelector("#info-mbti-left").style.transform = "translateY(-100px)";
-//         this.document.querySelector("#info-mbti-right").style.transform = "translateY(-100px)";
-//         triggered = false;
-//     }
-
-//     if (350 < scrollY && scrollY < 750 && !triggered) {
-//         console.log(Mapping(350, 750, 0.5, 1, scrollY));
-//         this.document.querySelector("#info-mbti-left").style.transform = "translateY(" + Mapping(350, 750, 0, -100, scrollY) + "px)";
-//         this.document.querySelector("#info-mbti-left").style.opacity = Mapping(350, 750, 0.3, 1, scrollY);
-
-//         this.document.querySelector("#info-mbti-right").style.transform = "translateY(" + Mapping(350, 750, 0, -100, scrollY) + "px)";
-//         this.document.querySelector("#info-mbti-right").style.opacity = Mapping(350, 750, 0.3, 1, scrollY);
-//     } else if (scrollY > 750) triggered = true;
-// });
+let trig_plan = false;
 
 window.addEventListener("scroll", scrollBehavior);
 
 function scrollBehavior() {
-    var element_mbti = this.document.querySelector("#info-mbti-left");
-    console.log(scrollY);
+    // console.log(scrollY);
 
     if (scrollY > 300) {
-        this.document.querySelector(".info-description").style.transform = "translateY(-200px)";
-        this.document.querySelector(".info-description").style.opacity = 0;
+        this.document.querySelector(".main-container").style.transform = "translateY(-200px)";
+        this.document.querySelector(".main-container").style.opacity = 0;
+
+        this.document.querySelector(".about-me-items").style.transform = "translateX(0px)";
+        this.document.querySelector(".about-me-items").style.opacity = 1;
     } else if (scrollY <= 300) {
-        this.document.querySelector(".info-description").style.transform = "translateY(0px)";
-        this.document.querySelector(".info-description").style.opacity = 1;
+        this.document.querySelector(".main-container").style.transform = "translateY(0px)";
+        this.document.querySelector(".main-container").style.opacity = 1;
+
+        this.document.querySelector(".about-me-items").style.transform = "translateX(-100px)";
+        this.document.querySelector(".about-me-items").style.opacity = 0;
     }
 
-    if (scrollY < 430) {
-        this.document.querySelector("#info-mbti-left").style.transform = "translateY(0px)";
-        this.document.querySelector("#info-mbti-right").style.transform = "translateY(0px)";
+    // project
+    if (scrollY > 3200) {
+        this.document.querySelector(".project-container-txt").style.transform = "translateY(0px)";
+        this.document.querySelector(".project-container-txt").style.opacity = 1;
+    } else {
+        this.document.querySelector(".project-container-txt").style.transform = "translateY(100px)";
+        this.document.querySelector(".project-container-txt").style.opacity = 0;
+    }
 
-        this.document.querySelector("#info-mbti-left").style.opacity = 0;
-        this.document.querySelector("#info-mbti-right").style.opacity = 0;
-    } else if (430 < scrollY && scrollY < 1500) {
-        this.document.querySelector("#info-mbti-left").style.transform = "translateY(-100px)";
-        this.document.querySelector("#info-mbti-left").style.opacity = 1;
+    // plan
 
-        this.document.querySelector("#info-mbti-right").style.transform = "translateY(-100px)";
-        this.document.querySelector("#info-mbti-right").style.opacity = 1;
-    } else if (scrollY > 1400) {
-        this.document.querySelector("#info-mbti-left").style.transform = "translateY(-200px)";
-        this.document.querySelector("#info-mbti-right").style.transform = "translateY(-200px)";
-
-        this.document.querySelector("#info-mbti-left").style.opacity = 0;
-        this.document.querySelector("#info-mbti-right").style.opacity = 0;
-    } else if (scrollY > 980) {
+    if (scrollY > 4200) {
+        if (trig_plan == false) {
+            console.log("triggered on");
+            trig_plan = true;
+            const elements_left = this.document.getElementsByClassName("item-left");
+            for (let i = 0; i < elements_left.length; i++) {
+                const element = elements_left.item(i);
+                element.style.opacity = 1;
+                element.style.transform = "translateX(0px)";
+            }
+            const elements_right = this.document.getElementsByClassName("item-right");
+            for (let i = 0; i < elements_right.length; i++) {
+                const element = elements_right.item(i);
+                element.style.opacity = 1;
+                element.style.transform = "translateX(0px)";
+            }
+        }
+    } else if (scrollY < 4200) {
+        if (trig_plan == true) {
+            console.log("triggered off");
+            trig_plan = false;
+            const elements_left = this.document.getElementsByClassName("item-left");
+            for (let i = 0; i < elements_left.length; i++) {
+                const element = elements_left.item(i);
+                element.style.opacity = 0;
+                element.style.transform = "translateX(-700px)";
+            }
+            const elements_right = this.document.getElementsByClassName("item-right");
+            for (let i = 0; i < elements_right.length; i++) {
+                const element = elements_right.item(i);
+                element.style.opacity = 0;
+                element.style.transform = "translateX(700px)";
+            }
+        }
     }
 }
